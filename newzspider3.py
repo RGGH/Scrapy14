d440a0bcd619e4753c236302af5c725e0eb3e981
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-12-24 15:26:25 +0000

    Update newzspider3.py

--- newzspider3.py
+++ newzspider3.py
@@ -20,7 +20,7 @@ class newzspider3(scrapy.Spider):
     'upgrade-insecure-requests': '1', 
     'user-agent': ' Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
     
-    #custom_settings = {'FEEDS':{'results3.csv':{'format':'csv'}}} # replace with MySQL
+    #custom_settings = {'FEEDS':{'results3.csv':{'format':'csv'}}} #  replace with MySQL
     
 
     def start_requests (self):
