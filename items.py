e89dbe18506cff7b8541213775f16b40d163302b
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-12-24 19:58:44 +0000

    Add files via upload

--- items.py
+++ items.py
@@ -4,9 +4,11 @@
 
 import scrapy
 
+
 class NewzzItem(scrapy.Item):
 
     publication = scrapy.Field()
-    headline = scrapy.Field()
+    title = scrapy.Field()
     author = scrapy.Field()
     story = scrapy.Field()
+    url = scrapy.Field()
