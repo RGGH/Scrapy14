8e1bcf8b09b1396ea47bed586384a8ac1d46195a
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-12-24 19:59:30 +0000

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for newz project
+# Scrapy settings for newzz project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,28 +7,28 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'newz'
+BOT_NAME = 'newzz'
 
-SPIDER_MODULES = ['newz.spiders']
-NEWSPIDER_MODULE = 'newz.spiders'
+SPIDER_MODULES = ['newzz.spiders']
+NEWSPIDER_MODULE = 'newzz.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'newz (+http://www.yourdomain.com)'
+#USER_AGENT = 'newzz (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 1
+#CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 8
+#DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 1
-CONCURRENT_REQUESTS_PER_IP = 1
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
@@ -44,17 +44,14 @@ CONCURRENT_REQUESTS_PER_IP = 1
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'newz.middlewares.NewzSpiderMiddleware': 543,
-#}
-
-
-
+SPIDER_MIDDLEWARES = {
+    'newzz.middlewares.NewzzSpiderMiddleware': 543,
+}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'newz.middlewares.NewzDownloaderMiddleware': 543,
+#    'newzz.middlewares.NewzzDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -63,19 +60,18 @@ CONCURRENT_REQUESTS_PER_IP = 1
 #    'scrapy.extensions.telnet.TelnetConsole': None,
 #}
 
+
 # Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-
-    'newzspider.pipelines.PricePipeline': 300,
-    'scrapy_jsonschema.JsonSchemaValidatePipeline': 100,
-
+    'newzz.pipelines.NewzzPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
+#AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 5
+#AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
 #AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
