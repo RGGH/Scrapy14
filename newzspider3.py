34db9b1ff2855166f5d926a831737a943c8f56a8
Red and Green <62441426+RGGH@users.noreply.github.com>
2021-01-08 17:21:01 +0000

    publication added
    need to add story still

--- newzspider3.py
+++ newzspider3.py
@@ -5,6 +5,7 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy import Request
+from scrapy.loader import ItemLoader
 import sys
 sys.path.insert(0,'..')
 from items import NewzzItem
@@ -12,7 +13,7 @@ from items import NewzzItem
 class newzspider3(scrapy.Spider):
 
     name = 'newzspider3'
-
+    start_urls = ['https://www.independent.co.uk/']
     headers={
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-encoding': 'gzip,',
@@ -24,15 +25,23 @@ class newzspider3(scrapy.Spider):
     'upgrade-insecure-requests': '1',
     'user-agent': ' Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
 
-    #custom_settings = {'FEEDS':{'results3.csv':{'format':'csv'}}} # replace with MySQL
-
     def start_requests (self):
         request = Request(url='https://www.independent.co.uk', headers=self.headers, callback=self.parse,dont_filter=True)
         yield request
 
     def parse(self, response):
-        self.logger.info('Got successful response from {}'.format(response.url))
+    
+        items = NewzzItem()
+        
+        links = response.xpath('//a/@href').getall()
 
+        for url in links[20:30]:
+            items['url'] = url
+            items['publication'] = 'independent'
+            items['author'] = 'author'
+            items['story'] = 'story'
+            items['title'] = 'title'
+            yield items
 
 
 # main driver
